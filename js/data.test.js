8a7dc38f1f6424f6e1850e9a4bd9495ea97ba244
Dustin Hardin <dhardin07@gmail.com>
2015-07-29 10:04:03 -0400

    added jspdf

--- js/data.test.js
+++ js/data.test.js
@@ -1,37 +1,46 @@
 data = [
 {
+	name: 'Robert Paulson',
+	department: 'Management',
+	parent: 0,
+	title: 'The Manager',
+	id: 1,
+	email: 'robert.paulson@management.net',
+	phone: '555-555-5555'
+},
+{
 	name: 'Bob',
 	department: 'Plumbing',
-	parent: 5,
+	parent: 1,
 	title: 'The Plumber',
-	id: 1,
+	id: 2,
 	email: 'bob@pluming.net',
 	phone: '555-555-5555'
 },
 {
 	name: 'Al',
 	department: 'Plumbing',
-	parent: 1,
+	parent: 2,
 	title: 'Assistant Plumber',
-	id: 2,
+	id: 3,
 	email: 'al@pluming.net',
 	phone: '555-555-5555'
 
 },{
 	name: 'Chaz',
 	department: 'Plumbing',
-	parent: 1,
+	parent: 2,
 	title: 'Assistant Plumber',
-	id: 3,
+	id: 4,
 	email: 'chaz@pluming.net',
 	phone: '555-555-5555'
 
 }, {
 	name: 'Ted',
 	department: 'Plumbing',
-	parent: 3,
+	parent: 4,
 	title: 'Rat Killer',
-	id: 4,
+	id: 5,
 	email: 'ted@pluming.net',
 	phone: '555-555-5555'
 
@@ -39,27 +48,27 @@ data = [
 {
 	name: 'Phil',
 	department: 'Electrical',
-	parent: 0,
+	parent: 1,
 	title: 'The Electrician',
-	id: 5,
+	id: 6,
 	email: 'phil@electrical.net',
 	phone: '555-555-5555'
 },
 {
 	name: 'Buzz',
 	department: 'Electrical',
-	parent: 5,
+	parent: 6,
 	title: 'Assistant Electrician',
-	id: 6,
+	id: 7,
 	email: 'buzz@electrical.net',
 	phone: '555-555-5555'
 
 },{
 	name: 'Sparky',
 	department: 'Electrical',
-	parent: 6,
+	parent: 7,
 	title: 'Assistant to the Assistant Electrician',
-	id: 7,
+	id: 8,
 	email: 'sparky@electrical.net',
 	phone: '555-555-5555'
 
