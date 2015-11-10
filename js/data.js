676632340088cf37062649b20f1cf1fe50a8a70d
Dustin Hardin <dhardin07@gmail.com>
2015-11-10 09:30:46 -0500

    conditional check for static names.

--- js/data.js
+++ js/data.js
@@ -10,20 +10,20 @@
 //
 var app = app || {};
 
-app.data = (function(){
+app.data = (function() {
     var stateMap = {
-        dataArr: [],
-        currentDataArrIndex: 0
-    },
-    getData, _getListItems, processData, saveData, _saveListItem, printError;
+            dataArr: [],
+            currentDataArrIndex: 0
+        },
+        getData, _getListItems, processData, saveData, _saveListItem, printError;
 
-    getData = function(dataArr, index, callback){
+    getData = function(dataArr, index, callback) {
         var url, guid, viewName, type, dataCallback;
 
-        if(!dataArr instanceof Array){
+        if (!dataArr instanceof Array) {
             return;
         }
-        if (index > dataArr.length){
+        if (index > dataArr.length) {
             return;
         }
 
@@ -31,7 +31,7 @@ app.data = (function(){
         url = dataArr[index].url;
         //data calls assume url ends with '/'
         //fix url if it dosn't end with '/'
-        if(!url.endsWith('/')){
+        if (!url.endsWith('/')) {
             url = url + '/';
         }
 
@@ -42,14 +42,14 @@ app.data = (function(){
         stateMap.dataArr = dataArr;
         stateMap.currentDataArrIndex = index || 0;
 
-        _getListItems(url, guid, type, function (results) {
-            if (dataCallback){
+        _getListItems(url, guid, type, function(results) {
+            if (dataCallback) {
                 dataCallback(results);
             }
             if (index < dataArr.length - 1) {
                 getData(dataArr, ++index, callback);
-            } else if(index == dataArr.length){
-                if(callback){
+            } else if (index == dataArr.length) {
+                if (callback) {
                     callback();
                 }
             }
@@ -57,15 +57,16 @@ app.data = (function(){
 
     };
 
-     // Begin Utility Method /_getListItems/
-    _getListItems = function (url, guid, type, callback) {
-        var results = [], soapEnv, body;
+    // Begin Utility Method /_getListItems/
+    _getListItems = function(url, guid, type, callback) {
+        var results = [],
+            soapEnv, body;
 
         soapEnv =
             '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\
                 <soap:Body>\
                     <GetListItems xmlns="http://schemas.microsoft.com/sharepoint/soap/">\
-                        <listName>'+guid+'</listName>\
+                        <listName>' + guid + '</listName>\
                     </GetListItems>\
                 </soap:Body>\
             </soap:Envelope>';
@@ -74,7 +75,7 @@ app.data = (function(){
 
         $.ajax({
             url: url + "_vti_bin/lists.asmx",
-            beforeSend: function (xhr) {
+            beforeSend: function(xhr) {
                 xhr.setRequestHeader('SOAPAction', 'http://schemas.microsoft.com/sharepoint/soap/GetListItems');
             },
             type: "POST",
@@ -82,7 +83,7 @@ app.data = (function(){
             contentType: "text/xml; charset=\"utf-8\"",
             data: soapEnv,
             tryCount: 3,
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
                 printError(XMLHttpRequest, textStatus, errorThrown);
                 this.tryCount++;
                 if (this.tryCount <= this.retryLimit) {
@@ -93,9 +94,9 @@ app.data = (function(){
                     callback(textStatus);
                 }
             },
-            complete: function (xData, status) {
+            complete: function(xData, status) {
                 var responseProperty = (xData.responseText ? 'responseText' : 'responseXML'),
-                 results = $(xData[responseProperty]).find('z\\:row');
+                    results = $(xData[responseProperty]).find('z\\:row');
 
                 if (callback) {
                     callback(results);
@@ -104,70 +105,65 @@ app.data = (function(){
         });
     };
     // End Utility Method /_getListItems/
-    
+
     // Begin Utilitiy method /_convertToStaticAttr/
-    _convertToStaticAttr= function(obj){
-        var staticObj = {}, key;
-        if(!app.config.static_names){
+    _convertToStaticAttr = function(obj) {
+        var staticObj = {},
+            key;
+        if (!app.config.static_names) {
             return;
         }
-        for (key in obj){
-            staticObj[app.config.static_names[key]] = obj[key];
+        for (key in obj) {
+            if (staticObj[app.config.static_names[key]]) {
+                staticObj[app.config.static_names[key]] = obj[key];
+            }
         }
         return staticObj;
     };
     // End Utility method /_convertToStaticAttr/
 
     // Begin Utility Method /saveData/
-    saveData = function(dataObjArr){
+    saveData = function(dataObjArr) {
         var url = dataObjArr.url,
-        data = dataObjArr.data,
-        guid = dataObjArr.guid,
-        callback = dataObjArr.callback,
-        method = dataObjArr.method;
+            data = dataObjArr.data,
+            guid = dataObjArr.guid,
+            callback = dataObjArr.callback,
+            method = dataObjArr.method;
 
-         _buildPayload(data, 0, {}, method, '', function (payload) {
-                _saveListItem(url, guid, payload, function(results){
-                    if(callback){
-                        callback(results);
-                    }
-                    
-                });
+        _buildPayload(data, 0, {}, method, '', function(payload) {
+            _saveListItem(url, guid, payload, function(results) {
+                if (callback) {
+                    callback(results);
+                }
+
             });
+        });
     };
     // End Utility Method /saveData/
 
-     // Begin Utility Method /_saveListItem/
-    _saveListItem = function (url, guid, payload, callback) {
+    // Begin Utility Method /_saveListItem/
+    _saveListItem = function(url, guid, payload, callback) {
         var results = [],
-            
-        // Create the SOAP request
-         soapEnv =
-            '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
-                + '<soap:Body>'
-                    + '<UpdateListItems xmlns="http://schemas.microsoft.com/sharepoint/soap/">'
-                    + '<listName>' + guid + '</listName>'
-                    + '<updates>'
-                        + '<Batch OnError="Continue" PreCalc="True">'
-                            + payload
-                        + '</Batch>'
-                    + '</updates>'
-                    + '</UpdateListItems>'
-                + '</soap:Body>'
-            + '</soap:Envelope>';
+
+            // Create the SOAP request
+            soapEnv =
+            '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' + '<soap:Body>' + '<UpdateListItems xmlns="http://schemas.microsoft.com/sharepoint/soap/">' + '<listName>' + guid + '</listName>' + '<updates>' + '<Batch OnError="Continue" PreCalc="True">' + payload + '</Batch>' + '</updates>' + '</UpdateListItems>' + '</soap:Body>' + '</soap:Envelope>';
 
         $.ajax({
             url: url + "/_vti_bin/lists.asmx",
-            beforeSend: function(xhr){
+            beforeSend: function(xhr) {
                 xhr.setRequestHeader('SOAPAction', 'http://schemas.microsoft.com/sharepoint/soap/UpdateListItems');
             },
             type: "POST",
             dataType: "xml",
             data: soapEnv,
             error: printError,
-            complete:  function(xData, status){
+            complete: function(xData, status) {
                 var error = $(xData.responseText).find('ErrorText').text(),
-                results = {data: $(xData.responseText).find('z\\:row'), error: error};
+                    results = {
+                        data: $(xData.responseText).find('z\\:row'),
+                        error: error
+                    };
 
                 if (callback) {
                     callback(results);
@@ -179,7 +175,7 @@ app.data = (function(){
     // End Utility Method /saveListItem/
 
     // Begin Utility Method /_buildPayload/
-    _buildPayload = function (arr, index, obj_map, method, payload, callback) {
+    _buildPayload = function(arr, index, obj_map, method, payload, callback) {
         var i, obj,
             method_map = {
                 'new': 'New',
@@ -190,16 +186,13 @@ app.data = (function(){
 
         payload = payload || "";
 
-        if (!(arr instanceof Array) 
-            || !(obj_map instanceof Object)
-            || !(method_map.hasOwnProperty(method.toLowerCase()))
-            ){
+        if (!(arr instanceof Array) || !(obj_map instanceof Object) || !(method_map.hasOwnProperty(method.toLowerCase()))) {
             return false;
         }
 
         method = method_map[method];
 
-        if(index < arr.length){
+        if (index < arr.length) {
             obj = arr[index];
             obj = _convertToStaticAttr(obj);
             for (key in obj) {
@@ -220,7 +213,7 @@ app.data = (function(){
             return payload;
         }
 
-   
+
     };
     // End Utility Method /_buildPayload/
 
@@ -230,7 +223,7 @@ app.data = (function(){
     };
 
     // Begin Utility Method /processData/
-     processData = function(results) {
+    processData = function(results) {
         var data = [{}],
             attrObj = {},
             i, j, attribute,
@@ -239,12 +232,12 @@ app.data = (function(){
 
         //repackage data into an array which each index
         //is an object with key value pairs
-        for (i = 0; i < results.length; i++){
+        for (i = 0; i < results.length; i++) {
             attrObj = {};
-            if(!results[i].attributes){
+            if (!results[i].attributes) {
                 continue;
             }
-            for (j = 0; j < results[i].attributes.length; j++){
+            for (j = 0; j < results[i].attributes.length; j++) {
                 attribute = results[i].attributes[j];
                 attrObj[attribute.name] = attribute.value;
             }
@@ -253,7 +246,7 @@ app.data = (function(){
 
         return data;
     };
-   // End Utility Method /processData/
+    // End Utility Method /processData/
 
     return {
         getData: getData,
@@ -262,11 +255,11 @@ app.data = (function(){
     };
 })();
 
-String.prototype.endsWith = function(suffix){
+String.prototype.endsWith = function(suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 }
 
-app.browser_util = (function(){
+app.browser_util = (function() {
     var configMap = {
             regex_encode_html: /[&"'><]/g,
             regex_encode_noamp: /["'><]/g,
@@ -286,7 +279,7 @@ app.browser_util = (function(){
     // See http://stackoverflow.com/questions/1912501/\
     // unescape-html-entities-in-javascript
     //
-    decodeHtml = function (str) {
+    decodeHtml = function(str) {
         return $('<div/>').html(str || '').text();
     };
     // End decodeHtml
@@ -295,30 +288,28 @@ app.browser_util = (function(){
     // This is single pass encoder for html entities and handles
     // an arbitrary number of characters
     //
-    encodeHtml = function (input_arg_str, exclude_amp) {
+    encodeHtml = function(input_arg_str, exclude_amp) {
         var
             input_str = String(input_arg_str),
-            regex, lookup_map
-        ;
+            regex, lookup_map;
 
         if (exclude_amp) {
             lookup_map = configMap.encode_noamp_map;
             regex = configMap.regex_encode_noamp;
-        }
-        else {
+        } else {
             lookup_map = configMap.html_encode_map;
             regex = configMap.regex_encode_html;
         }
         return input_str.replace(regex,
-                function (match, name) {
-                    return lookup_map[match] || '';
-                }
-            );
+            function(match, name) {
+                return lookup_map[match] || '';
+            }
+        );
     };
     // End encodeHtml
 
     // Begin encodeXml
-    encodeXml = function (string) {
+    encodeXml = function(string) {
         if (typeof string !== "string") {
             return string;
         }
@@ -333,11 +324,11 @@ app.browser_util = (function(){
     }
 })();
 
- // This method for finding specific nodes in the returned XML was developed by Steve Workman. See his blog post
-    // http://www.steveworkman.com/html5-2/javascript/2011/improving-javascript-xml-node-finding-performance-by-2000/
-    // for performance details.
-    $.fn.filterNode = function (name) {
-        return this.find('*').filter(function () {
-            return this.nodeName === name;
-        });
-    }; // End $.fn.filterNode
+// This method for finding specific nodes in the returned XML was developed by Steve Workman. See his blog post
+// http://www.steveworkman.com/html5-2/javascript/2011/improving-javascript-xml-node-finding-performance-by-2000/
+// for performance details.
+$.fn.filterNode = function(name) {
+    return this.find('*').filter(function() {
+        return this.nodeName === name;
+    });
+}; // End $.fn.filterNode
