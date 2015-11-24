4680e271c36cf4c04226053d38f1db1466102036
Dustin Hardin <dhardin07@gmail.com>
2015-11-24 14:35:03 -0500

    encode/decode url

--- js/routes/routes.js
+++ js/routes/routes.js
@@ -35,7 +35,7 @@ var Router = Backbone.Router.extend({
 
     orgchart: function(department) {
         var fetchingDataView, libraryView,
-        department = department ? decodeURI(department) : false;
+        department = department ? decodeURIComponent(department) : false;
         if (!app.state_map.fetched.items) {
             app.itemFetchData();
         }
